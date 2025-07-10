54d820633700fd1537b82b5bb4401bf02b2b7837
Austine Mahugu <austinemahugu8@gmail.com>
2025-07-10 15:22:48 +0300

    pycache

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-FILE_PATH = r"C:\Users\Hp\wrfout.nc"
-COUNTY_SHAPEFILE_PATH = r"C:\Users\Hp\gadm41_KEN_1.shp"
+FILE_PATH = r"C:\Users\Austoh\Desktop\code\Projects\wrfout_d01_2024-05-20_06_00_00"
+COUNTY_SHAPEFILE_PATH = r"C:\Users\Austoh\Desktop\code\Projects\gadm41_KEN_shp\gadm41_KEN_1.shp"
 
 STANDARD_PRESSURE_LEVELS = [1000, 850, 700, 500, 300, 200]
 
