bacf01d2b531a0e9d88e6ab24a90f2c9802ee105
Austine Mahugu <austinemahugu8@gmail.com>
2025-07-11 12:41:46 +0300

    Add: file id from drive and Make the varible and make it true

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-FILE_PATH = r"C:\Users\Austoh\Desktop\code\Projects\wrfout_d01_2024-05-20_06_00_00"
-COUNTY_SHAPEFILE_PATH = r"C:\Users\Austoh\Desktop\code\Projects\gadm41_KEN_shp\gadm41_KEN_1.shp"
+LOAD_FROM_DRIVE = True
+FILE_ID = "185VzXmp6HOfg_ursYktXLMMM1E3xxEc4"
 
+COUNTY_SHAPEFILE_PATH = r"C:\Users\Hp\gadm41_KEN_1.shp"
 STANDARD_PRESSURE_LEVELS = [1000, 850, 700, 500, 300, 200]
 
 CMAP_OPTIONS = {
