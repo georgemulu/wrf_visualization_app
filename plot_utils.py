368703be49f2af3a4811eb929ed2d435463425ba
Austine Mahugu <austinemahugu8@gmail.com>
2025-07-21 14:42:32 +0300

    Modified plot utils

--- plot_utils.py
+++ plot_utils.py
@@ -39,23 +39,22 @@ def create_plot(nc, var_type, time_idx=0, cmap='viridis', pressure_level=None):
             ax.add_feature(cfeature.OCEAN.with_scale('10m'), facecolor='lightblue')
             ax.add_feature(cfeature.LAKES.with_scale('10m'), facecolor='lightblue', edgecolor='blue')
             ax.add_feature(cfeature.LAND.with_scale('10m'), facecolor='#e0dccd')  # light beige land
-            
-            skip = (slice(None, None, 5), slice(None, None, 5))
-            
-            ax.barbs(to_np(lons[skip]), to_np(lats[skip]),
-                    to_np(u[skip]), to_np(v[skip]),
+
+            subset = 10
+            ax.barbs(to_np(lons[::subset, ::subset]), to_np(lats[::subset, ::subset]),
+                    to_np(u[::subset, ::subset]), to_np(v[::subset, ::subset]),
                     length=6, color='black', linewidth=0.5,
                     transform=ccrs.PlateCarree()
-                    )
+                    ) 
             current_data = wind_speed
 
         elif 'Temperature' in var_type:
             if var_type == 'Temperature (2m)':
                 temp = getvar(nc, 'T2', timeidx=time_idx) - 273.15
             else:
-                tk = getvar(nc, 'tk', timeidx=time_idx)
+                tk = getvar(nc, 'tk', timeidx=time_idx) -273.15
                 p = getvar(nc, 'pressure', timeidx=time_idx)
-                temp = interplevel(tk, p, pressure_level) - 273.15
+                temp = interplevel(tk, p, pressure_level)
             levels = np.linspace(np.min(temp), np.max(temp), 20)
             contour = ax.contourf(lons, lats, temp, levels=levels, cmap=cmap, transform=ccrs.PlateCarree())
             plt.colorbar(contour, ax=ax, label=f'Temperature (°C) at {pressure_level} hPa' if pressure_level else 'Temperature (°C)')
@@ -63,30 +62,29 @@ def create_plot(nc, var_type, time_idx=0, cmap='viridis', pressure_level=None):
 
         elif var_type == 'Rainfall':
             rain = getvar(nc, 'RAINNC', timeidx=time_idx)
+            rain += getvar(nc, 'RAINC', timeidx=time_idx)
             levels = np.linspace(0, 50, 11)
             contour = ax.contourf(lons, lats, rain, levels=levels, cmap=cmap, transform=ccrs.PlateCarree(), extend='max')
             plt.colorbar(contour, ax=ax, label='Rainfall (mm)')
             current_data = rain
 
-        elif 'Humidity' in var_type or var_type == 'Relative Humidity':
-            if var_type == 'Humidity (2m)':
-                if 'Q2' in nc.variables:
-                    hum = getvar(nc, 'Q2', timeidx=time_idx) * 1000
+        elif  var_type == 'Humidity (2m)':
+                    hum2 = getvar(nc, 'Q2', timeidx=time_idx) * 1000
                     label = 'Specific Humidity (g/kg)'
                     levels = np.linspace(0, 20, 11)
-                else:
-                    hum = getvar(nc, 'RH2', timeidx=time_idx)
-                    label = 'Relative Humidity (%)'
-                    levels = np.linspace(0, 100, 11)
-            else:
-                rh = getvar(nc, 'RH', timeidx=time_idx)
+                    contour = ax.contourf(lons, lats, hum2, levels=levels, cmap=cmap, transform=ccrs.PlateCarree())
+                    plt.colorbar(contour, ax=ax, label=f'{label} at {pressure_level} hPa' if pressure_level else label)
+                    current_data = hum2
+
+        elif var_type == 'Relative Humidity':
+                rh = getvar(nc, 'rh', timeidx=time_idx)
                 p = getvar(nc, 'pressure', timeidx=time_idx)
                 hum = interplevel(rh, p, pressure_level)
                 label = 'Relative Humidity (%)'
                 levels = np.linspace(0, 100, 11)
-            contour = ax.contourf(lons, lats, hum, levels=levels, cmap=cmap, transform=ccrs.PlateCarree())
-            plt.colorbar(contour, ax=ax, label=f'{label} at {pressure_level} hPa' if pressure_level else label)
-            current_data = hum
+                contour = ax.contourf(lons, lats, hum, levels=levels, cmap=cmap, transform=ccrs.PlateCarree())
+                plt.colorbar(contour, ax=ax, label=f'{label} at {pressure_level} hPa' if pressure_level else label)
+                current_data = hum
 
         ax.add_feature(cfeature.COASTLINE.with_scale('10m'))
         ax.add_feature(cfeature.BORDERS.with_scale('10m'), linestyle=':', edgecolor='gray')
