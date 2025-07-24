eae72b581ac4c82f99e2fabc42d4683cf69b57b0
Austine Mahugu <austinemahugu8@gmail.com>
2025-07-24 11:49:03 +0300

    Remove

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from backend import wrf_api
-
-app = FastAPI(title="WRF Weather API")
-
-app.include_router(wrf_api.router)
--- backend/wrf_utils.py
+++
@@ -1,9 +0,0 @@
-def extract_variable(nc, var_name: str, time_idx: int, level: int):
-    if var_name == "Rainfall":
-        return nc.RAINC[time_idx] + nc.RAINNC[time_idx]
-    elif var_name == "Temperature":
-        return nc.T[time_idx, level, :, :]
-    elif var_name =="Humidity":
-        return nc.RH[time_idx, level, :, :]
-    else:
-        raise ValueError("Unsupported variable")
